9103df367fa7cf8d67eba73ee82122d6db4dd3cf
Hamza Hebibovic <hamza.hebibovic@edu.fit.ba>
2022-12-27 10:38:58 +0100

    Initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {RouterModule, RouterOutlet} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { Navbar2Component } from './navbar2/navbar2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProfileComponent,
+    Navbar2Component
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+        {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'home', component: HomeComponent},
+      {path: 'profile', component: ProfileComponent},
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
